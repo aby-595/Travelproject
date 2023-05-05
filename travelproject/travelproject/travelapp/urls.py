d2fde6004a3aaa295832321c8f5e6a2801ce8e48
ABY D VARGHESE <sunnyvarghese25007@gmail.com>
2023-05-06 00:59:41 +0530

    Update urls.py

--- travelproject/travelproject/travelapp/urls.py
+++ travelproject/travelproject/travelapp/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
 
     path('',views.demo,name='demo'),
+    path('about.html', views.about, name='about'),
+    path('index.html', views.index, name='index')
 
 ]
